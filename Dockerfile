73e03aa9ef416d407340af0da93a354e7218e130
Joao Hinning <jphinning@gmail.com>
2022-09-14 19:20:09 -0300

    feat: Docker compose for prod created

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
-FROM node
+# Base image
+FROM node:18-alpine
 
-WORKDIR /app
+# Create app directory
+WORKDIR /usr/src/app
 
-COPY package.json ./
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
 
+# Install app dependencies
 RUN npm install
 
+# Bundle app source
 COPY . .
 
-EXPOSE  4000
+# Creates a "dist" folder with the production build
+RUN npm run build
 
-CMD ["npm", "start"]
+# Start the server using the production build
+CMD [ "node", "dist/main.js" ]
---
+++ src/config/database.config.ts
@@ -0,0 +1,29 @@
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+const env = process.env.NODE_ENV || 'development';
+const dotenv_path = path.resolve(process.cwd(), `./.${env}.env`);
+const result = dotenv.config({ path: dotenv_path });
+if (result.error) {
+  throw new Error(`config error - missing ${dotenv_path}`);
+}
+
+export const DatabaseConfig = {
+  type: 'mysql' as any,
+  database: process.env.MYSQL_DB_NAME || 'zenbit',
+  port: parseInt(process.env.MYSQL_PORT) || 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  timezone: 'Z',
+  logging: true,
+  autoLoadEntities: true,
+  synchronize: true,
+  migrationsRun: false,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/**/*{.ts,.js}'],
+  cli: { migrationsDir: 'src/migrations' },
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
